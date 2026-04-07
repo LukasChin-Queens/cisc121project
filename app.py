6c36137b62cd74b2bf833638e5850e617491d9b3
LukasChin-Queens <24dykm@queensu.ca>
2026-04-06 23:22:27 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,15 +7,22 @@ def linear_search_ui(numbers, target):
         arr = [int(x.strip()) for x in numbers.split(",")]
         # Convert input to integer
         target = int(target)
-
+        steps = ""
+        
         # Loop through each element in list
         for i in range(len(arr)):
+            
+            # Counting steps
+            steps += f"Step {i+1}: Checking index {i} (value = {arr[i]})\n"
+            
             # Check if current element matches target
             if arr[i] == target:
+                steps += f"\n✅ Found {target} at index {i}"
                 return f"Found at index {i}"
 
         # If loop finishes w/o finding target
-        return "Not found"
+        steps += f"\n❌ {target} not found in the list"
+        return steps
     
     except:
         # Invalid input
@@ -24,11 +31,20 @@ def linear_search_ui(numbers, target):
 # Gradio UI
 with gr.Blocks() as demo:
     gr.Markdown("## Linear Search App")
+    gr.Markdown("Enter a list of numbers and a target. The app will show each step of the search process.")
 
-    numbers_input = gr.Textbox(label="Enter numbers (comma separated)")
+    # Input
+    numbers_input = gr.Textbox(
+        label="Enter numbers (comma separated)",
+        placeholder="e.g. 1, 5, 3, 9"
+    )
     target_input = gr.Textbox(label="Enter number to search")
 
-    output = gr.Textbox(label="Result")
+    #Output
+    output = gr.Textbox(
+        label="Search Steps",
+        lines=10
+    )
 
     btn = gr.Button("Search")
 
